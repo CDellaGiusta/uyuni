037b17066f88fae25660c0cdd6ca339a2c00e921
Carlo Della Giusta <carlo.dellagiusta@suse.com>
2026-04-14 09:28:59 +0200

    microservices: add utlity classes

--- microservices/uyuni-java-common/src/main/java/com/suse/common/utilities/JsonUtilities.java
+++ microservices/uyuni-java-common/src/main/java/com/suse/common/utilities/JsonUtilities.java
@@ -11,16 +11,43 @@
 
 package com.suse.common.utilities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class JsonUtilities {
 
+    private JsonUtilities() {
+        // Utility classes should not have a public or default constructor
+    }
+
+    /**
+     * Creates an empty JSON file
+     *
+     * @return an empty JSON
+     */
     public static String createEmptyJson() {
         return "{}";
     }
 
+    /**
+     * Creates a JSON file with a first key-value pair
+     *
+     * @param key   to be added
+     * @param value to be associated to the key
+     * @return a created JSON file
+     */
     public static String createJson(String key, String value) {
         return addToJson("", key, value);
     }
 
+    /**
+     * Adds a key-value pair to an existing JSON file
+     *
+     * @param json  file already existing
+     * @param key   to be added
+     * @param value to be associated to the key
+     * @return the existing JSON file, with the added key-value pair
+     */
     public static String addToJson(String json, String key, String value) {
         String body = json;
         if (body.startsWith("{")) {
@@ -35,5 +62,48 @@ public class JsonUtilities {
         }
 
         return "{%s, \"%s\": \"%s\"}".formatted(body, key, value);
+    }
+
+    /**
+     * Decodes a simple JSON string into a key-value map
+     *
+     * @param simpleJsonString the string to decode
+     * @return a decoded key-value map
+     */
+    public static Map<String, String> decodeSimpleJsonString(String simpleJsonString) {
+        Map<String, String> result = new HashMap<>();
+
+        String body = trimChars(simpleJsonString, "\n {}\n ");
+
+        String[] splitArray = body.split("[:,]");
+
+        for (int i = 0; i < splitArray.length; i += 2) {
+            if (i < splitArray.length - 1) {
+                decodeResult(result, splitArray[i], splitArray[i + 1]);
+            }
+        }
+
+        return result;
+    }
+
+    private static void decodeResult(Map<String, String> result, String keyString, String valueString) {
+        String key = trimChars(keyString, "\n \"");
+        String value = trimChars(valueString, "\n \"");
+
+        result.put(key, value);
+    }
+
+    private static String trimChars(String str, String toTrimChars) {
+        for (int i = 0; i < toTrimChars.length(); i++) {
+            String toTrim = String.valueOf(toTrimChars.charAt(i));
+            while (str.startsWith(toTrim)) {
+                str = str.substring(1);
+            }
+
+            while (str.endsWith(toTrim)) {
+                str = str.substring(0, str.length() - toTrim.length());
+            }
+        }
+        return str;
     }
 }
